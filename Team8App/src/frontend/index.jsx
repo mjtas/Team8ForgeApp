3e0b3a43df186e98f5daedd7ccf5473688dbec3c
mjtas <101754279+mjtas@users.noreply.github.com>
2024-07-02 21:02:36 +1000

    Amend manifest.yml

--- Team8App/src/frontend/index.jsx
+++ Team8App/src/frontend/index.jsx
@@ -1,48 +1,110 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text, Table, Head, Row, Cell } from '@forge/react';
-import { invoke, requestConfluence } from '@forge/bridge';
+import ForgeReconciler, { Box, Heading, Image, Inline, Stack, Tab, Tabs, TabPanel, TabList, Text, xcss, Button} from '@forge/react';
+import { invoke } from '@forge/bridge';
 
 const App = () => {
-  const [leaderboardData, setLeaderboardData] = useState(null);
-  const [data, setData] = useState(null);
 
+  const cardData = [{
+    name: "Persephone",
+    imgurUrl: "pzpMdhI.png",
+    imgurDescription: "Persephone sitting in a flowerbed",
+    breed: "Domestic Short Hair",
+    age: "2 Years",
+    adoptionFee: 200
+  },{
+    name: "Walter",
+    imgurUrl: "1bCyWtV.png",
+    imgurDescription: "Walter, a ginger cat wears a burgundy bow tie",
+    breed: "Domestic Short Hair",
+    age: "4 Years",
+    adoptionFee: 200
+  },{
+    name: "Pickle",
+    imgurUrl: "5of4ryw.png",
+    imgurDescription: "Pickle is a smol tiny floof, wrapped in a pink blanket",
+    breed: "Domestic Short Hair",
+    age: "3 months",
+    adoptionFee: 250
+  },
+  {
+    name: "Pickle",
+    imgurUrl: "5of4ryw.png",
+    imgurDescription: "Pickle is a smol tiny floof, wrapped in a pink blanket",
+    breed: "Domestic Short Hair",
+    age: "3 months",
+    adoptionFee: 250
+  }
+]
 
-  const getConfluenceUserData = async () => {
-    const response = await requestConfluence('/wiki/rest/api/search/user?cql=type=user&maxResults=1000');
-    const data = await response.json();
-    return data.results;
-  };
+  const cardStyle = xcss({
+    backgroundColor: 'elevation.surface',
+    margin: 'space.200',
+    width: '25%',
+    padding: 'space.200',
+    boxShadow: 'elevation.shadow.overflow',
+    borderColor: 'color.border',
+    borderWidth: 'border.width',
+    borderStyle: 'solid',
+    borderRadius: 'border.radius',
+    ':hover': {
+      backgroundColor: 'elevation.surface.hovered',
+      width: '30%'
+    },  });
 
-  useEffect(() => {
-    setData = getConfluenceUserData();
-  })
+  const priceStyle = xcss({
+    backgroundColor: 'color.background.accent.purple.subtler',
+    width: '90%',
+    marginBottom: 'space.200',
+    borderRadius: 'border.radius'
+  });
 
-  useEffect(() => {
-    // Fetch the leaderboard data using the 'invoke' function
-    // invoke('getLeaderboard').then(setLeaderboardData);
-    invoke('getConfluence').then(setData);
-  }, []);
+  function Card(pet) {
+    return (
+      <>
+        <Box xcss={cardStyle}>
+          <Stack alignInline="center" space="space.200" >
+            <Heading as="h2">{pet.name}</Heading>
+            <Image src={"https://i.imgur.com/"+ pet.imgurUrl} size="large" alt={pet.imgurDescription}></Image>
+            <Box>
+              <Stack alignInline="center" space="space.0" >
+                <Box><Text>{pet.breed}</Text></Box>
+                <Box><Text>{pet.age}</Text></Box>
+              </Stack>
+            </Box>
+            <Box xcss={priceStyle}>
+              <Stack alignInline="center" space="space.0">
+                <Box><Text>Adoption fee</Text></Box>
+                <Button>Sign Up</Button>
+                <Box><Text>${pet.adoptionFee}</Text></Box>
+              </Stack>
+            </Box>
+          </Stack>
+        </Box>
+      </>
+    )
+  }
+
+  function CardGroup(pets) {
+    return (
+      <>
+        <Box>
+          <Inline alignInline="center">
+            {pets.map(pet => (Card(pet)))}
+          </Inline>
+        </Box>
+      </>
+    )
+  }
 
   return (
     <>
-      {console.log(data)}
-      <Text>Leaderboard</Text>
-      {leaderboardData ? (
-        <Table>
-          <Head>
-            <Cell>User</Cell>
-            <Cell>Tickets Bought</Cell>
-          </Head>
-          {leaderboardData.map(({ email, count }) => (
-            <Row key={email}>
-              <Cell>{email}</Cell>
-              <Cell>{count}</Cell>
-            </Row>
-          ))}
-        </Table>
-      ) : (
-        <Text>Loading...</Text>
-      )}
+      <Button>Leaderboard</Button>
+      <Heading as="h1">Volunteer Opportunity</Heading>
+      <Tabs id="default">
+      <Box xcss={{width: '100%'}}>
+        {CardGroup(cardData)}
+      </Box>
+      </Tabs>
     </>
   );
 };
